709aadba42f44642694747163844e45f8e27161c
rohanwasudeo <107766866+rohanwasudeo@users.noreply.github.com>
2025-10-31 19:22:33 +0530

    Add src\App.js

---
+++ src\App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="container vh-100 d-flex justify-content-center align-items-center bg-light">
+      <div className="text-center animate__animated animate__fadeInDown">
+        <h1 className="display-3 fw-bold mb-3">Hello Duniya</h1>
+        <p className="lead text-secondary">Welcome to your simple React app!</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
